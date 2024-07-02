ea273dd4e40c4da76d090d541cca77d1e97e3cda
demian <132276126+demianarc@users.noreply.github.com>
2024-07-02 19:25:43 +0200

    Update app.py to gpt4o

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def process_with_gpt(reviews, language):
 
     try:
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
+            model="gpt-4o",
             messages=[
                 {"role": "system", "content": role_message},
                 {"role": "user", "content": prompt}
